ea7bdddd3591e3d7979f4114857a83162d2fa2b9
sungyujeon <sungyujeon91@gmail.com>
2022-10-08 23:00:13 +0900

    feat: 자동차 이동 로직 구현

---
+++ src/main/java/racingcar/model/Position.java
@@ -0,0 +1,14 @@
+package racingcar.model;
+
+public class Position {
+
+    private int position = 0;
+
+    public int get() {
+        return this.position;
+    }
+
+    public void increase() {
+        this.position++;
+    }
+}
